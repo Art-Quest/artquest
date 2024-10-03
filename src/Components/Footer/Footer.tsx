e83cf5c6270f8ab1ef53237b7a13e509ad8d715f
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-10-03 21:31:43 +0100

    revamped landing page

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,16 +2,15 @@ import React from 'react'
 import twitter from "../../assets/twitter-brands-solid.svg"
 import discord from "../../assets/discord-brands-solid.svg"
 import github from "../../assets/github-brands-solid.svg"
-import "./footer.css"
 
 export const Footer = () => {
   return (
     <footer>
-        <div className='footer flex items-center justify-center'>
-            <div className="footer-container">
+        <div className='flex items-center justify-evenly'>
+            <div className="hidden sm:block text-white">
                 <p>&copy; 2024 ArtQuest. All rights reserved. </p>
             </div>
-            <div className="footer-text">
+            <div className="text-white">
                 Built and developed on Tron
             </div>
             {/* <div className="footerss-social">
