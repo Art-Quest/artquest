372e126d802c918683f75181ef6e9de30b6fa65c
Sukuna <ajeemmanuel221@gmail.com>
2024-10-05 11:07:53 +0100

    [fix] - resolved merge conflicts

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import "../Header/header.css";
 import search from "../../assets/magnifying-glass-solid.svg";
 import Logo from "../../assets/logo-no-bg.png"
@@ -15,28 +15,24 @@ export const Header = () => {
                   <Link to="/"><img  src={Logo} alt="" /></Link>
                 </div>
 
-        {/* Menu (hidden on mobile, visible on md screens and up) */}
-        <div className="hidden md:flex md:space-x-16">
-          <div className="cursor-pointer quests">
-            <h1 className="text-gray-800 hover:text-blue-500">Quests</h1>
-          </div>
-          <div className="cursor-pointer quests">
-            {/* <Link to="/profile"> */}
-            <h1 className="text-gray-800 hover:text-blue-500">Profile</h1>
-            {/* </Link> */}
-          </div>
-          <div className="cursor-pointer quests">
-            <h1 className="text-gray-800 hover:text-red-500">Hot 🔥</h1>
-          </div>
-        </div>
+                {/* Menu (hidden on mobile, visible on md screens and up) */}
+                <div className="hidden md:flex md:space-x-16">
+                    <div className="cursor-pointer quests">
+                        <h1 className="text-gray-800 hover:text-blue-500">Quests</h1>
+                    </div>
+                    <div className="cursor-pointer quests">
+                        <h1 className="text-gray-800 hover:text-blue-500">Profile</h1>
+                    </div>
+                    <div className="cursor-pointer quests">
+                        <h1 className="text-gray-800 hover:text-red-500">Hot 🔥</h1>
+                    </div>
+                </div>
 
-        {/* Connect Wallet button (hidden on mobile, visible on md screens and up) */}
-        <div className="hidden md:block">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
-            Connect Wallet
-          </button>
+                {/* Connect Wallet button (hidden on mobile, visible on md screens and up) */}
+                <div className="hidden md:block">
+                    <button className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700'>Connect Wallet</button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    );
+}
