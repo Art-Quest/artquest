8d053c5c70ab9c30cfa2d3628a2c5a5f01e6fc91
Sukuna <ajeemmanuel221@gmail.com>
2024-10-05 12:26:57 +0100

    changed footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import twitter from "../../assets/twitter-brands-solid.svg"
 import discord from "../../assets/discord-brands-solid.svg"
 import github from "../../assets/github-brands-solid.svg"
+import "./footer.css"
 
 export const Footer = () => {
   return (
     <footer>
-        <div className='bg-black flex items-center justify-evenly'>
-            <div className="hidden sm:block text-white">
+        <div className='footer-cover flex items-center justify-evenly'>
+            <div className="cover-text hidden sm:block text-white">
                 <p>&copy; 2024 ArtQuest. All rights reserved. </p>
             </div>
-            <div className="text-white">
+            <div className="cover-text text-white">
                 Built and developed on Tron
             </div>
             {/* <div className="footerss-social">
